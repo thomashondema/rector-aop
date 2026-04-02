eacf7b15e400d447e41d569d62f2b282e2de89bf
Thomas Hondema <thomashondema@live.com>
2026-04-02 14:08:31 +0200

    Change the way files are kept in sync

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,25 +1,81 @@
+#!/usr/bin/env sh
+
+SRC="/app_src"
+DST="/app"
+SOURCE_CODE="${SOURCE_DIR:-src}"
+
+link_entry() {
+  src="$1"
+  rel="${src#"$SRC"/}"
+  dst="$DST/$rel"
+
+  mkdir -p "$(dirname "$dst")"
+  if [ "$src" != "$dst" ]; then
+    ln -sfn "$src" "$dst"
+  fi
+}
+
+copy_entry() {
+  src="$1"
+  rel="${src#"$SRC"/}"
+  dst="$DST/$rel"
+
+  mkdir -p "$(dirname "$dst")"
+
+  if [ -d "$src" ]; then
+    rsync -a --delete "$src/" "$dst/"
+  else
+    rsync -a "$src" "$dst"
+  fi
+}
+
+run_rector_if_file() {
+  src="$1"
+  rel="${src#"$SRC"/}"
+  dst="$DST/$rel"
+
+  if [ -f "$dst" ]; then
+    cd "$DST" && php vendor/bin/rector process "$dst" --config=rector-build.php
+  fi
+}
+
 sync_all() {
-  rm -rf /app/*
-  rsync -a \
-    --exclude .git \
-    --exclude .idea \
-    --exclude .vscode \
-    --exclude .build \
-    /app_src/ /app/
-  cd /app && php vendor/bin/rector process --config=rector-build.php
+  rm -rf "${DST:?}"/*
+
+  find "$SRC" -mindepth 1 -maxdepth 1 | while read -r entry; do
+    name="$(basename "$entry")"
+
+    if [ "$name" = "$SOURCE_CODE" ]; then
+      copy_entry "$entry"
+    else
+      link_entry "$entry"
+    fi
+  done
 }
 
 sync_one() {
   changed="$1"
-  rel="${changed#/app_src/}"
-  dst="/app/$rel"
+  rel="${changed#"$SRC"/}"
+  top="${rel%%/*}"
 
+  # If the change is inside the source code directory, copy it
+  case "$rel" in
+    "$SOURCE_CODE"/*|"$SOURCE_CODE")
+      if [ -e "$changed" ]; then
+        copy_entry "$changed"
+        run_rector_if_file "$changed"
+      else
+        rm -rf "${DST:?}/$rel"
+      fi
+      return
+      ;;
+  esac
+
+  # Otherwise just keep a symlink in place
   if [ -e "$changed" ]; then
-    mkdir -p "$(dirname "$dst")"
-    rsync -a "$changed" "$dst"
-    cd /app && php /app/vendor/bin/rector process "$dst" --config=rector-build.php
+    link_entry "$changed"
   else
-    rm -f "$dst"
+    rm -f  "${DST:?}/$rel"
   fi
 }
 
@@ -27,7 +83,7 @@ echo "Initial sync..."
 sync_all
 
 echo "Watching for changes..."
-inotifywait -mr -e close_write,create,delete,move --format '%w%f' /app_src | \
-while read file; do
+inotifywait -mr -e close_write,create,delete,move --format '%w%f' "$SRC" | \
+while read -r file; do
   sync_one "$file"
 done
